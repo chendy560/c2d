6e9d5283f9c3a1575238f03b5258e0bbd5970c1a
chendayu <dayu_chen@kingdee.com>
2019-02-28 14:49:53 +0800

    初步支持数组

---
+++ processor/src/test/java/com/chendayu/c2d/processor/declaration/ArrayTest.java
@@ -0,0 +1,48 @@
+package com.chendayu.c2d.processor.declaration;
+
+import com.chendayu.c2d.processor.Declaration;
+import com.chendayu.c2d.processor.DeclarationType;
+import com.chendayu.c2d.processor.Declarations;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.chendayu.c2d.processor.DeclarationType.ARRAY;
+import static com.chendayu.c2d.processor.DeclarationType.NUMBER;
+import static com.chendayu.c2d.processor.DeclarationType.STRING;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayTest extends AbstractDeclarationTest {
+
+    @Test
+    public void testCollection() {
+        List<Declaration> declarations = compile(ArrayTestSupport.class);
+
+        assertThat(declarations).hasSize(5);
+        Declaration intArray = declarations.get(0);
+        checkArrayDeclaration(intArray, NUMBER);
+
+        Declaration charArray = declarations.get(1);
+        assertThat(charArray.getType()).isEqualTo(STRING);
+
+        Declaration doubleArray = declarations.get(2);
+        checkArrayDeclaration(doubleArray, NUMBER);
+
+        Declaration stringArray = declarations.get(3);
+        checkArrayDeclaration(stringArray, STRING);
+
+        Declaration stringListArray = declarations.get(4);
+        checkArrayDeclaration(stringListArray, ARRAY);
+
+        Declarations.ArrayDeclaration stringList = (Declarations.ArrayDeclaration) stringListArray;
+        checkArrayDeclaration(stringList.getComponentType(), STRING);
+
+    }
+
+    private void checkArrayDeclaration(Declaration declaration, DeclarationType componentType) {
+        assertThat(declaration.getType()).isEqualTo(ARRAY);
+        Declarations.ArrayDeclaration arrayDeclaration = (Declarations.ArrayDeclaration) declaration;
+        Declaration ct = arrayDeclaration.getComponentType();
+        assertThat(ct.getType()).isEqualTo(componentType);
+    }
+}
---
+++ processor/src/test/java/com/chendayu/c2d/processor/declaration/ArrayTestSupport.java
@@ -0,0 +1,13 @@
+package com.chendayu.c2d.processor.declaration;
+
+import java.util.List;
+
+@DeclarationTest
+public class ArrayTestSupport {
+
+    @DeclarationTest
+    public void test(int[] i, char[] c, double[] d, String[] s,
+                     List<String>[] lss) {
+
+    }
+}
