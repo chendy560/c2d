57eec8f9fde92cd23b4c27f36006917323246b45
chendayu <chendy560@foxmail.com>
2019-03-11 23:13:36 +0800

    整理文档输出

--- processor/src/main/java/com/chendayu/c2d/processor/DocWriter.java
+++ processor/src/main/java/com/chendayu/c2d/processor/DocWriter.java
@@ -1,79 +1,91 @@
 package com.chendayu.c2d.processor;
 
 import java.io.Writer;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeMap;
 
 public class DocWriter {
 
-    private final Writer writer;
+    private final TreeMap<String, Declaration> declarationMap = new TreeMap<>();
+
+    private final AdocWriter adoc;
 
     public DocWriter(Writer writer) {
-        this.writer = writer;
+        this.adoc = new AdocWriter(writer);
     }
 
     public void printDoc(Warehouse warehouse) {
 
-        TreeMap<String, Declaration> declarationMap = new TreeMap<>();
+        writeTitle(warehouse);
 
-        Collection<Resource> resources = warehouse.getResources();
-        String applicationName = warehouse.getApplicationName();
-        AdocWriter adoc = new AdocWriter(writer);
+        writeResources(warehouse.getResources());
 
-        adoc.title0(applicationName + " API 文档")
-                .title1("资源");
+        writeDeclarations();
+    }
+
+    private void writeResources(Collection<Resource> resources) {
+        adoc.title1("资源");
 
         for (Resource resource : resources) {
             adoc.title2(resource.getName());
 
             SortedSet<Action> actions = resource.getActions();
             for (Action action : actions) {
-                String name = action.getName();
-                adoc.title3(name);
-                adoc.appendLines(action.getDescription());
-
-                adoc.sourceCodeBegin("http")
-                        .append(action.getMethod().name()).space().append(action.getPath())
-                        .newLine()
-                        .sourceCodeEnd();
-
-                List<Property> pathVariables = action.getPathVariables();
-                if (!pathVariables.isEmpty()) {
-                    adoc.title4("路径参数");
-                    for (Property pathVariable : pathVariables) {
-                        saveDeclaration(pathVariable.getDeclaration(), declarationMap);
-                    }
-                    parameterTable(pathVariables, adoc);
-                }
-
-                List<Property> urlParameters = action.getUrlParameters();
-                if (!urlParameters.isEmpty()) {
-                    adoc.title4("URL参数");
-                    for (Property property : urlParameters) {
-                        saveDeclaration(property.getDeclaration(), declarationMap);
-                    }
-                    parameterTable(urlParameters, adoc);
-                }
-
-                Property requestBody = action.getRequestBody();
-                if (requestBody != null) {
-                    adoc.title4("请求Body");
-                    appendType(requestBody.getDeclaration(), adoc);
-                    saveDeclaration(requestBody.getDeclaration(), declarationMap);
-                    adoc.dualNewLine();
-                }
-
-                Property responseBody = action.getResponseBody();
-                if (responseBody != null) {
-                    adoc.title4("响应");
-                    appendType(responseBody.getDeclaration(), adoc);
-                    saveDeclaration(responseBody.getDeclaration(), declarationMap);
-                    adoc.dualNewLine();
-                }
-
-                adoc.dualNewLine();
+                writeActions(action);
             }
         }
+    }
 
+    private void writeActions(Action action) {
+        String name = action.getName();
+        adoc.title3(name);
+        adoc.appendLines(action.getDescription());
+
+        adoc.sourceCodeBegin("http")
+                .append(action.getMethod().name()).space().append(action.getPath())
+                .newLine()
+                .sourceCodeEnd();
+
+        List<Property> pathVariables = action.getPathVariables();
+        if (!pathVariables.isEmpty()) {
+            adoc.title4("路径参数");
+            for (Property pathVariable : pathVariables) {
+                saveDeclaration(pathVariable.getDeclaration());
+            }
+            parameterTable(pathVariables);
+        }
+
+        List<Property> urlParameters = action.getUrlParameters();
+        if (!urlParameters.isEmpty()) {
+            adoc.title4("URL参数");
+            for (Property property : urlParameters) {
+                saveDeclaration(property.getDeclaration());
+            }
+            parameterTable(urlParameters);
+        }
+
+        Property requestBody = action.getRequestBody();
+        if (requestBody != null) {
+            adoc.title4("请求Body");
+            writeType(requestBody.getDeclaration());
+            saveDeclaration(requestBody.getDeclaration());
+            adoc.dualNewLine();
+        }
+
+        Property responseBody = action.getResponseBody();
+        if (responseBody != null) {
+            adoc.title4("响应");
+            writeType(responseBody.getDeclaration());
+            saveDeclaration(responseBody.getDeclaration());
+            adoc.dualNewLine();
+        }
+
+        adoc.dualNewLine();
+    }
+
+    private void writeDeclarations() {
         adoc.title1("对象结构");
 
         for (Declaration declaration : declarationMap.values()) {
@@ -87,13 +99,13 @@ public class DocWriter {
                     List<Property> typeParameters = od.getTypeParameters();
                     if (!typeParameters.isEmpty()) {
                         adoc.title4("类型参数");
-                        parameterTable(typeParameters, adoc);
+                        parameterTable(typeParameters);
                     }
 
                     Collection<ObjectProperty> properties = od.getProperties();
                     if (!properties.isEmpty()) {
                         adoc.title4("字段");
-                        parameterTable(properties, adoc);
+                        parameterTable(properties);
                     }
                     break;
                 case ENUM:
@@ -102,7 +114,7 @@ public class DocWriter {
                     List<Property> constants = ed.getConstants();
                     if (!constants.isEmpty()) {
                         adoc.title4("常量列表");
-                        parameterTable(constants, adoc);
+                        parameterTable(constants);
                     }
                     break;
                 default:
@@ -111,7 +123,13 @@ public class DocWriter {
         }
     }
 
-    private void saveDeclaration(Declaration declaration, Map<String, Declaration> declarationMap) {
+    private void writeTitle(Warehouse warehouse) {
+        String applicationName = warehouse.getApplicationName();
+
+        adoc.title0(applicationName + " API 文档");
+    }
+
+    private void saveDeclaration(Declaration declaration) {
         DeclarationType type = declaration.getType();
         if (type == DeclarationType.OBJECT) {
             ObjectDeclaration od = (ObjectDeclaration) declaration;
@@ -122,11 +140,11 @@ public class DocWriter {
             declarationMap.put(od.getName(), od);
 
             for (Declaration typeArg : od.getTypeArgs()) {
-                saveDeclaration(typeArg, declarationMap);
+                saveDeclaration(typeArg);
             }
 
             for (ObjectProperty property : od.getProperties()) {
-                saveDeclaration(property.getDeclaration(), declarationMap);
+                saveDeclaration(property.getDeclaration());
             }
 
             return;
@@ -136,7 +154,7 @@ public class DocWriter {
 
             Declarations.ArrayDeclaration ad = (Declarations.ArrayDeclaration) declaration;
             Declaration componentType = ad.getComponentType();
-            saveDeclaration(componentType, declarationMap);
+            saveDeclaration(componentType);
             return;
         }
 
@@ -151,7 +169,7 @@ public class DocWriter {
         }
     }
 
-    private void parameterTable(Collection<? extends Property> parameters, AdocWriter adoc) {
+    private void parameterTable(Collection<? extends Property> parameters) {
 
         adoc.tableBegin()
                 .columnBegin().append("名称").space()
@@ -161,7 +179,7 @@ public class DocWriter {
         for (Property p : parameters) {
             adoc.columnBegin().append(p.getName()).newLine();
             adoc.columnBegin();
-            appendType(p.getDeclaration(), adoc);
+            writeType(p.getDeclaration());
             adoc.newLine();
             adoc.columnBegin()
                     .appendLines(p.getDescription());
@@ -171,7 +189,7 @@ public class DocWriter {
     }
 
 
-    private void appendType(Declaration d, AdocWriter adoc) {
+    private void writeType(Declaration d) {
         DeclarationType type = d.getType();
         switch (type) {
             case STRING:
@@ -207,7 +225,7 @@ public class DocWriter {
                 Declarations.ArrayDeclaration ad = (Declarations.ArrayDeclaration) d;
                 Declaration cd = ad.getComponentType();
                 adoc.append("Array<");
-                appendType(cd, adoc);
+                writeType(cd);
                 adoc.append('>');
                 break;
             case OBJECT:
@@ -218,7 +236,7 @@ public class DocWriter {
                 } else {
                     adoc.link(od.getHash(), od.getName()).append('<');
                     for (int i = 0; i < typeArgs.size(); i++) {
-                        appendType(typeArgs.get(i), adoc);
+                        writeType(typeArgs.get(i));
                         if (i != typeArgs.size() - 1) {
                             adoc.append(" ,");
                         }
